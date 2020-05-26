28c3496d42651ac26d7e02b4adaad5d40b233b29
Richard Clamp <richardc@unixbeard.net>
2020-05-26 15:23:53 +0100

    Load metadata from directory things
    Sometimes we're downloading older stuff from thingiverse that doesn't
    have a parseable LICENSE.txt.
    
    Here we add a metadata file called =miniverse.json= where you can
    specify the id, title, author, and license which overrides what we would
    parse from the ~LICENSE.txt~.
    
    This means we have a way of storing
    things even when the metadata parse would otherwise have failed and
    called them unthings.
    
    Currently this is factored a little oddly - the json is turned into a
    license string that can be regexed; instead it should probably be more
    like the metadata loader loads from json or parses the license, but this
    gets us going.

--- lib/miniverse/thing/directory.rb
+++ lib/miniverse/thing/directory.rb
@@ -1,5 +1,9 @@
+require 'json'
+
 class Miniverse::Thing::Directory < Miniverse::Thing
   def get_license
+    return fake_license if has_metadata
+
     return nil unless File.exists?("#{path}/LICENSE.txt")
     File.read("#{path}/LICENSE.txt")
   end
@@ -8,5 +12,24 @@ class Miniverse::Thing::Directory < Miniverse::Thing
     target = "#{miniverse}/#{miniverse_path}"
     puts "Renaming directory #{path} into #{target}"
     File.rename(path, target)
+  end
+
+  private
+  def metadata_path
+    "#{path}/miniverse.json"
+  end
+
+  def has_metadata
+    File.exists? metadata_path
+  end
+
+  def load_metadata
+    return nil unless has_metadata
+    JSON.parse(File.read(metadata_path))
+  end
+
+  def fake_license
+    metadata = load_metadata
+    "#{metadata.fetch('title', 'Unknown')} (https://www.thingiverse.com/thing:#{metadata['id']}) by #{metadata.fetch('author', 'Unknown')} is licensed under the #{metadata.fetch('license', 'Unknown')} license.\n"
   end
 end
